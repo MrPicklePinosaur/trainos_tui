da4c9088990c81e075a6746009bc5089a9accd6f
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-12-06 17:49:16 -0500

    properly receiving c struct from kernel

--- src/main.rs
+++ src/main.rs
@@ -19,20 +19,37 @@ fn main() {
         match port.read(serial_buf.as_mut_slice()) {
             Ok(t) => {
                 msg.extend_from_slice(&serial_buf[..t]);
+                // let out = msg.iter().map(|ch| format!("{ch:x}")).collect::<Vec<_>>().join(" ");
+                // println!("cur msg: {out}");
 
                 if msg.len() < START_MSG_BYTES + LENGTH_BYTES {
                     // wait for msg header to be read
                     continue;
                 }
 
+                // ignore if not magic bytes
+                if msg[0] != 0x69 || msg[1] != 0x69 {
+                    panic!("invalid magic bytes");
+                }
+
                 let expected_msg_len: [u8; 4] = msg
                     [START_MSG_BYTES..START_MSG_BYTES + LENGTH_BYTES]
                     .try_into()
                     .unwrap();
-                let expected_msg_len = u32::from_le_bytes(expected_msg_len);
-                println!("expect message of length {expected_msg_len}");
-                // let out = serial_buf[..t].iter().map(|ch| format!("{ch:x}")).collect::<Vec<_>>().join(" ");
-                // println!("{out}");
+                let expected_msg_len = u32::from_le_bytes(expected_msg_len) as usize;
+
+                if msg.len() < START_MSG_BYTES + LENGTH_BYTES + expected_msg_len {
+                    continue;
+                }
+
+                let out = msg[START_MSG_BYTES + LENGTH_BYTES
+                    ..START_MSG_BYTES + LENGTH_BYTES + expected_msg_len]
+                    .iter()
+                    .map(|ch| format!("{ch:x}"))
+                    .collect::<Vec<_>>()
+                    .join(" ");
+                println!("done: {out}");
+                msg.drain(..START_MSG_BYTES + LENGTH_BYTES + expected_msg_len); // finished processing
             },
             Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
             Err(e) => eprintln!("{:?}", e),
