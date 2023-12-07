8ef5a9be2c3ec614c6b1d5bd3b5b52525f7a7f28
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-12-06 19:27:08 -0500

    use tokio

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::{
 
 use bincode::config::Options;
 use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc;
 
 use crate::protocol::*;
 
@@ -14,9 +15,33 @@ const START_MSG_BYTES: usize = 2; // magic bytes 0x6969
 const LENGTH_BYTES: usize = 4; // using u32 for msg length
 const TYPE_BYTES: usize = 4; // using u32 for msg type
 
-fn main() {
-    //let bincode_config = bincode::config::DefaultOptions::new().with_big_endian();
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = mpsc::channel::<SerialMessage>(32);
 
+    tokio::spawn(serial_conn(tx));
+
+    while let Some(serial_msg) = rx.recv().await {
+        // deserialize the binary data
+        match serial_msg.msg_type {
+            1 => {
+                let sensor: SensorMsg = bincode::deserialize(&serial_msg.data).unwrap();
+                println!("{sensor:?}");
+            },
+            _ => {
+                eprintln!("invalid type {}", serial_msg.msg_type);
+            },
+        }
+    }
+}
+
+pub struct SerialMessage {
+    data: Vec<u8>,
+    msg_len: u32,
+    msg_type: u32,
+}
+
+async fn serial_conn(tx: mpsc::Sender<SerialMessage>) {
     let mut port = serialport::new("/dev/ttyUSB0", 115_200)
         .timeout(Duration::from_millis(10))
         .open()
@@ -43,15 +68,15 @@ fn main() {
                 let msg_len: [u8; 4] = msg[START_MSG_BYTES..START_MSG_BYTES + LENGTH_BYTES]
                     .try_into()
                     .unwrap();
-                let msg_len = u32::from_le_bytes(msg_len) as usize;
+                let msg_len = u32::from_le_bytes(msg_len);
 
                 let msg_type: [u8; 4] = msg
                     [START_MSG_BYTES + LENGTH_BYTES..START_MSG_BYTES + LENGTH_BYTES + TYPE_BYTES]
                     .try_into()
                     .unwrap();
-                let msg_type = u32::from_le_bytes(msg_type) as usize;
+                let msg_type = u32::from_le_bytes(msg_type);
 
-                if msg.len() < START_MSG_BYTES + LENGTH_BYTES + TYPE_BYTES + msg_len {
+                if msg.len() < START_MSG_BYTES + LENGTH_BYTES + TYPE_BYTES + msg_len as usize {
                     continue;
                 }
 
@@ -63,22 +88,19 @@ fn main() {
                 //     .join(" ");
                 // println!("len = {msg_len}, type = {msg_type}, msg = {out}");
 
-                let out = &msg[START_MSG_BYTES + LENGTH_BYTES + TYPE_BYTES
-                    ..START_MSG_BYTES + LENGTH_BYTES + TYPE_BYTES + msg_len];
+                let data: Vec<u8> = msg[START_MSG_BYTES + LENGTH_BYTES + TYPE_BYTES
+                    ..START_MSG_BYTES + LENGTH_BYTES + TYPE_BYTES + msg_len as usize]
+                    .to_vec();
 
-                // deserialize the binary data
-                match msg_type {
-                    1 => {
-                        let sensor: SensorMsg = bincode::deserialize(&out).unwrap();
-                        println!("{sensor:?}");
-                    },
-                    _ => {
-                        eprintln!("invalid type {msg_type}");
-                    },
-                }
+                let serial_msg = SerialMessage {
+                    data,
+                    msg_len,
+                    msg_type,
+                };
+                tx.send(serial_msg).await.unwrap();
 
                 // finished processing
-                msg.drain(..START_MSG_BYTES + LENGTH_BYTES + TYPE_BYTES + msg_len);
+                msg.drain(..START_MSG_BYTES + LENGTH_BYTES + TYPE_BYTES + msg_len as usize);
             },
             Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
             Err(e) => eprintln!("{:?}", e),
